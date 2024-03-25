23d769824a27f5c1db501c701880c0aadbd65ef8
ezecodes <>
2024-03-25 09:58:55 +0100

    add profile route handler

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,16 @@ const quizRoute = require("./quiz");
 const classRoomRoute = require("./classRoom");
 const studentRoute = require("./student");
 const assessmentRoute = require("./assessment");
+const profileRoute = require("./profile");
 const {authenticateUser} = require("./middleware/auth")
 
 router.use("/auth", authRoute);
 router.use("/topic", authenticateUser, topicRoute);
 router.use("/tts", authenticateUser, ttsRoute);
+router.use("/profile", authenticateUser, profileRoute);
 router.use("/assessment", authenticateUser, assessmentRoute);
 router.use("/student", authenticateUser, studentRoute);
 router.use("/classRoom", authenticateUser, classRoomRoute);
 router.use("/quiz", authenticateUser, quizRoute);
-
-// router.use("/me", authenticateUser, getUser)
 
 module.exports = router;
