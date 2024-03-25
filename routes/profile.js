e1ced879d5020c5f7d329b11a139439e08f5dbd1
ezecodes <>
2024-03-25 11:38:04 +0100

    get teacher profile

--- routes/profile.js
+++ routes/profile.js
@@ -7,11 +7,12 @@ const { catchAsyncErrors } = require('./middleware/errors');
 const ApiError = require("../utils/errors")
 
 router.get('/', catchAsyncErrors(async function(req, res, next) {
-  const {id} = req.params
-  const find = Users.findByPk(id, {
+  const {id} = req.user
+  const find = await Users.findByPk(id, {
   	attributes: ["fullName", "email", "isEmailVerified", "role", "imgPath", "createdAt"]
   })
-  return res.status(201).json({
+  console.log(find)
+  return res.status(200).json({
     message: "Profile retrieved",
     profile: find
   })
